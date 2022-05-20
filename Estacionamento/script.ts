4bc591c4520465338716c1d97597799b82cab4a4
Cesar Augusto <cesinhalius@hotmail.com>
2022-05-20 16:09:39 -0300

    Implementando funções ao arquivo do typescript

--- Estacionamento/script.ts
+++ Estacionamento/script.ts
@@ -1,6 +1,37 @@
+interface Veiculo{
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
+
+
 (function() {
     const $ = (query : string): HTMLInputElement | null  => document.querySelector(query);
 
+
+    function patio(){
+        function ler(){}
+        function adicionar(veiculo : Veiculo){
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+            <button class="delete" data-placa"${veiculo.placa}"></button>
+            </td>
+            `;
+
+            $("patio")?.appendChild(row);
+        }
+        function remover(){}
+        function salvar(){}
+        function render(){}
+
+        return {ler, adicionar, remover, salvar, render}
+
+    }
 
     $("#cadastrar")?.addEventListener("click", () => {
         const nome = $("#nome")?.value;
@@ -10,5 +41,7 @@
             alert("Os campos de nome e placa são obrigatorios");
             return;
         }
+
+        patio().adicionar({nome, placa, entrada: new Date()});
     })
 })();
